7d190cc083a12734221e72bc4220376b5f5d5c9e
Hilmi Wicaksono <hilmiwicaksonop@gmail.com>
2021-06-25 00:45:00 +0700

    :sparkles: :hammer: done fetching spotify client credentials token

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+require('dotenv').config()
 const { default: fetch } = require("node-fetch");
 
+const PLAYLIST_ID = process.env.SPOTIFY_PLAYLIST_ID 
 
-fetch("http://fakeapi.jsonparseronline.com/posts")
-    .then(response => response.json())
-    .then(json => console.log(json))
+function getToken() {
+    
+    const encodeClientKey = process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+    const encodedClientKey = Buffer.from(encodeClientKey, 'utf-8').toString('base64')    
+
+    return fetch("https://accounts.spotify.com/api/token", {
+        method : "post",
+        headers : {
+            'Authorization' : "Basic " + encodedClientKey,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body : 'grant_type=client_credentials'
+    })
+    .then(response => (response.json()))
+    .then(json => ((json.access_token)))
+}
+
+//next api call
+async function test(){
+    const testing = await getToken()
+}
