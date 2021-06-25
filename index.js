d4c141d5c2ff87a82c7e04f194adf04f9828472c
Hilmi Wicaksono <hilmiwicaksonop@gmail.com>
2021-06-26 00:35:42 +0700

    :sparkles: added new txt file for testing and tried scraping ap40

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
-const { cheerio } = require('cheerio');
-const { default: fetch } = require("node-fetch");
+const cheerio  = require('cheerio');
+const fetch = require("node-fetch");
+const fs = require('fs')
 
 function getToken() {
     
@@ -21,17 +22,46 @@ function getToken() {
 }
 
 async function scrapeAP40(){
-    const AP40Response = fetch("http://asiapop40.com").then()
+    // const test = await fetch('https://reddit.com/')
+    // const body = await test.text()
+    // return cheerio.load(body)
+
+    const AP40HTMl = await fetch("http://asiapop40.com")
+                                .then(response => response.text())
+                                .catch(err => console.log(err))
+
+    const $ = cheerio.load(AP40HTMl)
+
+    //buat percobaan pertama, pake first dulu, nanti baru pake each
+    const chartSong = $('.accordion-item').first()
+    const chartSongRank = chartSong.find('.chart-track-rank').text()
+    const chartSongTitle = chartSong.find('.chart-track-title').text()
+    const chartSongArtist = chartSong.find('.chart-artist-title').text()
+    // return chartItem
+    const chartData = chartSongRank + '\n' + chartSongTitle + '\n' + chartSongArtist
+    fs.writeFileSync('./test.txt', chartData, 'utf8')
+
+
+    // const allChartArray = []
+    // $('.accordion-item').each((i, chartItem) => {
+    //     const chartNode = $(chartItem)
+    //     allChartArray.push(chartNode.html())
+    // })
+    // fs.writeFileSync('./test.txt', allChartArray.toString(), 'utf8')
+
+    
 }
 
 async function mainFunction(){
 
     console.log('Scraping Asia Pop 40 \'s website ...')
+    const top40List = await scrapeAP40()
+    // console.log(top40List)
 
-    console.log('Getting Spotify Token ...')
-    const token = await getToken()
+    // console.log('Getting Spotify Token ...')
+    // const token = await getToken()
  
-    console.log('Updating Spotify Playlist')
+    // console.log('Updating Spotify Playlist')
 
 
 
