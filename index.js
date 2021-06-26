da7d64a23d28e4c2f1331a27296b1422afd5cfcd
Hilmi Wicaksono <hilmiwicaksonop@gmail.com>
2021-06-26 15:55:20 +0700

    :sparkles: :pencil2: made new chart.json file and done scraping the AP40's site, also rewrote some function

--- index.js
+++ index.js
@@ -3,28 +3,35 @@ const cheerio  = require('cheerio');
 const fetch = require("node-fetch");
 const fs = require('fs')
 
-function getToken() {
+/** 
+ * function that takes spotify client credential flow's token
+ *  
+ */
+const getToken = () => {
     
     const encodeClientKey = process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
     const encodedClientKey = Buffer.from(encodeClientKey, 'utf-8').toString('base64')    
 
     return fetch("https://accounts.spotify.com/api/token", {
-        method : "post",
-        headers : {
-            'Authorization' : "Basic " + encodedClientKey,
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body : 'grant_type=client_credentials'
-    })
-    .then(response => response.json())
-    .then(responseJson => responseJson)
-    .catch(err => err)
+                method : "post",
+                headers : {
+                    'Authorization' : "Basic " + encodedClientKey,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body : 'grant_type=client_credentials'
+            })
+            .then(response => response.json())
+            .then(responseJson => responseJson)
+            .catch(err => err)
 }
 
-async function scrapeAP40(){
-    // const test = await fetch('https://reddit.com/')
-    // const body = await test.text()
-    // return cheerio.load(body)
+/**
+ * function that scrapes Asia Pop 40 's website .
+ * takes texts from classes "chart-track-<title or rank or artists>" ,
+ * change them into array and then into json object, and finally
+ * stores it into ./chart.json
+ */
+const scrapeAP40 = async () => {
 
     const AP40HTMl = await fetch("http://asiapop40.com")
                                 .then(response => response.text())
@@ -32,27 +39,40 @@ async function scrapeAP40(){
 
     const $ = cheerio.load(AP40HTMl)
 
-    //buat percobaan pertama, pake first dulu, nanti baru pake each
-    const chartSong = $('.accordion-item').first()
-    const chartSongRank = chartSong.find('.chart-track-rank').text()
-    const chartSongTitle = chartSong.find('.chart-track-title').text()
-    const chartSongArtist = chartSong.find('.chart-artist-title').text()
-    // return chartItem
-    const chartData = chartSongRank + '\n' + chartSongTitle + '\n' + chartSongArtist
-    fs.writeFileSync('./test.txt', chartData, 'utf8')
+    let charts = []
 
+    $('.accordion-item').each((i, chartItem) => {
+        let chartNode = $(chartItem)
+        let chartSongRank = chartNode.find('.chart-track-rank').text()
 
-    // const allChartArray = []
-    // $('.accordion-item').each((i, chartItem) => {
-    //     const chartNode = $(chartItem)
-    //     allChartArray.push(chartNode.html())
-    // })
-    // fs.writeFileSync('./test.txt', allChartArray.toString(), 'utf8')
+        let chartSongTitle = chartNode.find('.chart-track-title').text()
+        chartSongTitle = chartSongTitle.split(' ')
+        chartSongTitle.pop() // removing the '-' when taking the title
+        chartSongTitle = chartSongTitle.join(' ')
 
+        let chartSongArtists = []
+        chartNode.find('.chart-artist-title').children().each((i, artist) => {
+            let artistNode = $(artist)
+            chartSongArtists.push(artistNode.text())
+        })
+                
+        let chartData = {
+            rank : chartSongRank,
+            title : chartSongTitle,
+            artists : chartSongArtists
+        }
+    
+        charts.push(chartData)
+    })
+
+    fs.writeFileSync('./chart.json', JSON.stringify(charts), 'utf8')
     
 }
 
-async function mainFunction(){
+/**
+ * main function that runs all of the functions above
+ */
+const mainFunction = async () => {
 
     console.log('Scraping Asia Pop 40 \'s website ...')
     const top40List = await scrapeAP40()
